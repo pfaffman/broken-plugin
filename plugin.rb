47e81b8cf84a0a0e785e80d483c6600bbce1d4f5
Jay Pfaffman <pfaffman@gmail.com>
2019-11-08 10:02:43 -0800

    try a 303

--- plugin.rb
+++ plugin.rb
@@ -66,7 +66,7 @@ after_initialize do
                            }, SiteSetting.thinkific_api_key)
       puts "Signing in payload!!! #{payload}"
 
-      return redirect_to thinkific_sso_url(payload)
+      return redirect_to thinkific_sso_url(payload) status: 303
     end
 
     def generate_url(url, params = {})
